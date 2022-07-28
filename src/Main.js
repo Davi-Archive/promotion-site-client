d915a727a86de22518ed42c2ba28d0604eaf6df2
davi38 <davi4alves@gmail.com>
2022-07-28 17:59:35 -0300

    commit inicial

---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import './Main.css';
+
+export default function Main() {
+  return (
+    <>
+    <div className="body-header"></div>
+    <div className='body-main'></div>
+    <div className='body-bottom'></div>
+    </>
+  );
+}
