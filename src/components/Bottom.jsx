68ac7c9f116dc8009fff232b3fb69b01d9069e62
davi38 <davi4alves@gmail.com>
2022-07-29 22:18:18 -0300

    texto terminado

--- src/components/Bottom.jsx
+++ src/components/Bottom.jsx
@@ -1,9 +1,37 @@
 import '../styles/Bottom.css'
+import Logo from './Logo'
 
 export default function Bottom(){
     return(
-        <div className="bottom bg-amber-600 text-center">
-            Placeholder-Bottom
+        <>
+        <div className="bottom">
+            <h1 className='text-2xl font-semibold'>SÓ QUEM VENDE CURTE AS <br /> EXPERIÊNCIAS MAIS DIFERENTONAS!</h1>
+            <br />
+            <div className='texto-informativo'>
+                <p>*Campanha válida de 01/02 a 31/03/2020. Vendas via Business Center e para o
+                 Setor Público não serão válidas. A apuração do mês
+                </p>
+                <p>
+                    de agosto será retroativa.  Imagens meramente ilustrativas.
+                    Realizamos todos os esforços para garantir a precisão das informações, mas não
+                </p>
+                <p>somos responsáveis por qualquer conteúdo editorial,
+                   fotográfico ou por erros tipográficos. Todas as imagens são apenas para fins
+                </p>
+                <p>ilustrativos. Para especificações completas de produto, informações de serviço e garantias,
+                visite www.umbrindenaeuropa.com/saiba.
+                </p>
+                <p>Não fazemos nenhuma representação ou garantia em relação a produtos ou serviços de terceiros.
+                Outros nomes de empresas, produtos
+                </p>
+                <p>e serviços podem ser marcas registradas ou marcas de serviço de terceiros &copy; 2020.
+                Todos os direitos reservados.
+                </p>
         </div>
+        </div>
+        <div className='bottom-logo'>
+            <Logo></Logo>
+        </div>
+        </>
     )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,19 @@ import '../styles/Footer.css'
 
 export default function Footer(){
     return(
-        <div className="footer bg-amber-600 text-center">
-            Placeholder-Footer
+        <div className='barra-final'>
+        <div className="duvidas">
+            <span>
+                Dúvidas? Envie um e-mail para <a href="mailto:atendimento@umbrindenaeuropa.com.br">
+                atendimento@umbrindenaeuropa.com.br</a>
+            </span>
+
+        </div>
+        <div className='imagens-ilustrativas-texto'>
+            <span>
+                Imagens meramente ilustrativas. Promoção válida de 01/02 a 31/03/2020.
+            </span>
+        </div>
         </div>
     )
 }
