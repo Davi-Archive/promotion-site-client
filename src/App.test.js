a3e393ff0c4be443b92c95a390efcbc0e8fa4be4
davi38 <davi4alves@gmail.com>
2022-07-31 16:24:24 -0300

    Updates

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Body from './Body';
-
-test('renders learn react link', () => {
-  render(<Body />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
